4581a0f2d342604112cf36b4f8ab5ea8d6a52a7d
Filip Danielsson <filip.danielsson@tui.se>
2019-06-18 10:33:05 +0200

    YR jobtitles

--- src/views/staff/season.js
+++ src/views/staff/season.js
@@ -9,28 +9,28 @@ const Season = props => {
 
     const firstJobTitles = props.jobTitles.filter(
         m =>
-            m.season === props.application.season &&
+            (m.season === props.application.season || m.season === 'YR') &&
             m.destination === props.application.firstDest &&
             (m.jobFamily === props.jobFamily ||
                 (props.jobFamily.toLowerCase().indexOf('team manager') >= 0 && m.jobFamily.toLowerCase().indexOf('team manager') >= 0))
     )
     const secondJobTitles = props.jobTitles.filter(
         m =>
-            m.season === props.application.season &&
+            (m.season === props.application.season || m.season === 'YR') &&
             m.destination === props.application.secondDest &&
             (m.jobFamily === props.jobFamily ||
                 (props.jobFamily.toLowerCase().indexOf('team manager') >= 0 && m.jobFamily.toLowerCase().indexOf('team manager') >= 0))
     )
     const thirdJobTitles = props.jobTitles.filter(
         m =>
-            m.season === props.application.season &&
+            (m.season === props.application.season || m.season === 'YR') &&
             m.destination === props.application.thirdDest &&
             (m.jobFamily === props.jobFamily ||
                 (props.jobFamily.toLowerCase().indexOf('team manager') >= 0 && m.jobFamily.toLowerCase().indexOf('team manager') >= 0))
     )
     const fourthJobTitles = props.jobTitles.filter(
         m =>
-            m.season === props.application.season &&
+            (m.season === props.application.season || m.season === 'YR') &&
             m.destination === props.application.fourthDest &&
             (m.jobFamily === props.jobFamily ||
                 (props.jobFamily.toLowerCase().indexOf('team manager') >= 0 && m.jobFamily.toLowerCase().indexOf('team manager') >= 0))
