426476829bbe3fdfac6148308ba176afdafcbbfd
Filip Danielsson <filip.danielsson@tui.se>
2019-06-24 09:39:13 +0200

    removed childcare level

--- src/views/staff/education.js
+++ src/views/staff/education.js
@@ -1,27 +1,7 @@
 import React from 'react'
 import { Card, CardBody, CardHeader } from 'reactstrap'
 import TextInput from '../../components/textInput'
-import Select from '../../components/select'
 import { Col } from 'reactstrap'
-
-const childCareLevels = [
-    {
-        id: 'Level 3 or above',
-        name: 'Level 3 or above'
-    },
-    {
-        id: 'Level 2 or equivalent',
-        name: 'Level 2 or equivalent'
-    },
-    {
-        id: 'Other',
-        name: 'Other'
-    },
-    {
-        id: 'None',
-        name: 'None'
-    }
-]
 
 const Education = props => {
     return (
@@ -38,34 +18,6 @@ const Education = props => {
                                 label="Education"
                                 value={props.staff.education}
                                 onChange={props.handleStaffField}
-                            />
-                        </Col>
-
-                        <Col sm="12" md="6" lg="6" xl="6" className="form-group form-group-select">
-                            <label htmlFor="childCare">Childcare Level</label>
-
-                            <Select
-                                id="childCare"
-                                valueKey="id"
-                                labelKey="name"
-                                className="form-control"
-                                options={childCareLevels}
-                                onChange={v => {
-                                    props.handleStaffSelect('childCare', v, 'id')
-                                }}
-                                disabled={true}
-                                value={props.staff.childCare === '' ? null : props.staff.childCare}
-                                placeholder="Select"
-
-                                //    id="childCare"
-
-                                //    valueKey="id"
-                                //    labelKey="name"
-                                //    className="form-control"
-                                //    options={props.childCareLevels}
-                                //    onChange={(v) => { props.handleStaffSelect('reasonForResignment', v, 'id') }}
-                                //    value={props.staff.resignmentReasons === '' ? null : props.staff.resignmentReasons}
-                                //    placeholder="childCare"
                             />
                         </Col>
                     </div>
